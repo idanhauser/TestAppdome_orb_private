4341f8b3b6c8934b36be296ddd51dd5afd125588
Idan Hauser <idanh@appdome.com>
2023-01-10 12:03:57 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -4,9 +4,10 @@
 mkdir files
 echo "Downloading android file"
 wget "${APPFILE}" -O files/vanilla.apk
-VAR="${SIGNOVERRIDS}"
 if [[ -n $VAR ]]; then
     echo "Downloading sign overrides"
     wget "$VAR" -O .files/sign_overrides.json
+else
+    echo "no sign overrides"
 fi
 ls files
