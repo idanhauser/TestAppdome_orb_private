cfc396b2b5c95cccb50fbd609a236588207b665d
Idan Hauser <idanh@appdome.com>
2023-01-10 12:08:23 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -4,7 +4,7 @@
 mkdir output
 VAR="${GOOGLEPLAYSIGN}"
 ls
-if [[ -n "$VAR" ]]; then
+if [[ -z "$VAR" ]]; then
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.apk --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
