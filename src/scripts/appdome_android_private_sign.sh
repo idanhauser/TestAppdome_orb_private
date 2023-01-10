0775936613e87e65f62c1c120ebe8d16b6b618a1
Idan Hauser <idanh@appdome.com>
2023-01-10 10:10:42 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -6,15 +6,15 @@ VAR="${GOOGLEPLAYSIGN}"
 if [[ -n "$VAR" ]]; then
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .appdome_runner/files/vanilla.apk --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides .appdome_runner/files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .files/vanilla.apk --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides .files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .appdome_runner/files/vanilla.apk --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides .appdome_runner/files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .files/vanilla.apk --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides .files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     fi
 else
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .appdome_runner/files/vanilla.apk --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .files/vanilla.apk --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .appdome_runner/files/vanilla.apk --private_signing --signing_fingerprint "${FINGERPRINT}" --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app .files/vanilla.apk --private_signing --signing_fingerprint "${FINGERPRINT}" --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     fi
 fi
