f5abd0fb0ce80778cb41166e8ae16699749dc28e
Idan Hauser <idanh@appdome.com>
2023-01-10 09:47:00 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-name: 
-command: |
+
 mkdir output
 VAR="${GOOGLEPLAYSIGN}"
 if [[ -n $VAR ]]; then
     VAR="${GOOGLEPLAYSIGN}"
     if [ $VAR = true ]; then
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --sign_overrides ./files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --sign_overrides ./files/sign_overrides.json --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --sign_overrides ./files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
-    fi
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --sign_overrides ./files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        fi
 else
     VAR="${GOOGLEPLAYSIGN}"
     if [ $VAR = true ]; then
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --google_play_signing --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
     else
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
-    fi
+        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key $APPDOME_API_KEY --fusion_set_id << pipeline.parameters.fusion-set-id >> --app ./files/vanilla.apk --private_signing --signing_fingerprint  << pipeline.parameters.signing-fingerprint >> --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+        fi
 fi
