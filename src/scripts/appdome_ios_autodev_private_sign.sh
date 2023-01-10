f59a2178de035721e1ab2dc758ecd89d607cd7e5
Idan Hauser <idanh@appdome.com>
2023-01-10 16:51:13 +0200

    fixed files

--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -5,7 +5,7 @@ mkdir output
 VAR="${SIGNOVERRIDS}"
 ls
 if [[ -z "$VAR" ]]; then
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.apk --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles/ --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.ipa --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles/ --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
 else
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.apk --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles/ --output ./output/appdome_vanilla.apk --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.ipa --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles/ --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
 fi
--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -3,13 +3,33 @@
 
 mkdir files
 echo "Downloading android file"
-wget "${APPFILE}" -O files/vanilla.apk
+if [[ "${APPFILE}" == *".ipa" ]]; then
+    wget "${APPFILE}" -O files/vanilla.ipa
+elif [[ "${APPFILE}" == *".apk" ]]; then
+    wget "${APPFILE}" -O files/vanilla.apk
+else
+    echo "Error: Unsupported file format"
+fi
 if [[ -n "${SIGNOVERRIDS}" ]]; then
     echo "Downloading sign overrides"
     wget "${SIGNOVERRIDS}" -O files/sign_overrides.json
 fi
 if [[ -n "${KEYSTORE}" ]]; then
     echo "Downloading keystore"
-    wget "${KEYSTORE}" -O files/keystore.keystore
+    if [[ "${KEYSTORE}" == *".keystore" ]]; then
+        wget "${KEYSTORE}" -O files/keystore.keystore
+    elif [[ "${KEYSTORE}" == *".p12" ]]; then
+        wget "${KEYSTORE}" -O files/keystore.p12
+    else
+        echo "Error: Unsupported file format"
+    fi
+fi
+if [[ -n "${PROVISIONING_PROFILES}" ]]; then
+    echo "Downloading provisioning-profiles"
+    wget "${PROVISIONING_PROFILES}" -O files/provisioning-profiles/
+fi
+if [[ -n "${ENTITLEMENTS}" ]]; then
+    echo "Downloading entitlements"
+    wget "${ENTITLEMENTS}" -O files/entitlements/
 fi
 ls files
