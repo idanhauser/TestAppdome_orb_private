4285fbda493813fab42360a3e6a2807378612e78
Idan Hauser <idanh@appdome.com>
2023-01-10 13:42:49 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -4,11 +4,11 @@
 mkdir files
 echo "Downloading android file"
 wget "${APPFILE}" -O files/vanilla.apk
-if [[ -z "${SIGNOVERRIDS}" ]]; then
+if [[ -n "${SIGNOVERRIDS}" ]]; then
     echo "Downloading sign overrides"
     wget "${SIGNOVERRIDS}" -O .files/sign_overrides.json
 fi
-if [[ -z "${KEYSTORE}" ]]; then
+if [[ -n "${KEYSTORE}" ]]; then
     echo "Downloading keystore"
     wget "${KEYSTORE}" -O .files/keystore.p12
 fi
