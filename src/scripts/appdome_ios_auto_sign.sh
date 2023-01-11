aaf81af81ee5cbd7ba546a7bb18a6e9df174fb36
Idan Hauser <idanh@appdome.com>
2023-01-11 11:43:04 +0200

    cahnge the script to get the app from: "$(basename "$APPFILE")"

--- src/scripts/appdome_ios_auto_sign.sh
+++ src/scripts/appdome_ios_auto_sign.sh
@@ -5,7 +5,7 @@ mkdir output
 VAR="${SIGNOVERRIDS}"
 ls
 if [[ -z "$VAR" ]]; then
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.ipa --sign_on_appdome --keystore files/keystore.p12 --keystore_pass "${KEYSTORE_PASS}" --provisioning_profiles files/provisioning_profiles/ --entitlements files/entitlements/ --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/keystore.p12 --keystore_pass "${KEYSTORE_PASS}" --provisioning_profiles files/provisioning_profiles/ --entitlements files/entitlements/ --sign_overrides files/sign_overrides.json --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
 else
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/vanilla.ipa --sign_on_appdome --keystore files/keystore.p12 --keystore_pass "${KEYSTORE_PASS}" --provisioning_profiles files/provisioning_profiles/ --entitlements files/entitlements/ --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/keystore.p12 --keystore_pass "${KEYSTORE_PASS}" --provisioning_profiles files/provisioning_profiles/ --entitlements files/entitlements/ --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
 fi
