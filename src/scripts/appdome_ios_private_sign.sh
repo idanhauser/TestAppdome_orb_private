e774c700932c692f7dd6e38b484eb95c211a5ef7
Idan Hauser <idanh@appdome.com>
2023-01-11 12:08:38 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_ios_private_sign.sh
+++ src/scripts/appdome_ios_private_sign.sh
@@ -5,7 +5,7 @@ mkdir output
 VAR="${SIGNOVERRIDS}"
 ls
 if [[ -z "$VAR" ]]; then
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles/ --sign_overrides files/"$(basename "$SIGNOVERRIDS")" --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles/ --sign_overrides files/"$(basename "$SIGNOVERRIDS")" --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf
 else
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles/ --output ./output/appdome_vanilla.ipa --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles/ --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf
 fi
