a11c41b19bcd55d2ffd5516ffe9c81e3be392b28
Idan Hauser <idanh@appdome.com>
2023-01-11 18:12:46 +0200

    ANDROID_AUTO_DEV_SIGNING - no sign

--- src/scripts/appdome_android_autodev_private_sign.sh
+++ src/scripts/appdome_android_autodev_private_sign.sh
@@ -4,7 +4,7 @@
 mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
-if [[ -z "$VAR" ]]; then
+if [[ -n "$VAR" ]]; then
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
--- src/scripts/appdome_ios_private_sign.sh
+++ src/scripts/appdome_ios_private_sign.sh
@@ -4,7 +4,7 @@
 mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
-if [[ -z "$VAR" ]]; then
+if [[ -n "$VAR" ]]; then
     echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 else
