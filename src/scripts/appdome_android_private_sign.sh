dcdb054e21089ee3a78b016776c2e55ed7a82f78
Idan Hauser <idanh@appdome.com>
2023-01-11 18:07:50 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -8,15 +8,19 @@ ls
 if [[ -n "$VAR" ]]; then
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
+        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     else
+        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     fi
 else
     VAR="${GOOGLEPLAYSIGN}"
     if [ "$VAR" = true ]; then
+        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     else
+        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     fi
 fi
--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -5,7 +5,9 @@ mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
 if [[ -z "$VAR" ]]; then
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 else
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 fi
